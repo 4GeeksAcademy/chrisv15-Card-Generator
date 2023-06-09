7a4876d537be7644ed7dbda7fd75d221acdf0d48
chrisv15 <128597332+chrisv15@users.noreply.github.com>
2023-06-09 07:48:16 +0000

    car-generator

--- src/app.js
+++ src/app.js
@@ -7,5 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let suits = ["♦", "♥", "♠", "♣"];
+
+  let randomize = array => {
+    const randomNum = Math.floor(Math.random() * array.length);
+    return array[randomNum];
+  };
+
+  let selectedSuit = randomize(suits);
+
+  document.getElementById("cardTop").innerHTML = selectedSuit;
+  document.getElementById("cardMiddle").innerHTML = randomize(numbers);
+  document.getElementById("cardBottom").innerHTML = selectedSuit;
+
+  if (["♦", "♥"].includes(selectedSuit)) {
+    document.getElementById("cardTop").style.color = "red";
+    document.getElementById("cardBottom").style.color = "red";
+  }
 };
